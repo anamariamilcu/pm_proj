4da492fe2473a3f84d39a735e18873cc9f457342
Ana-Maria Milcu <anamariamilcu@gmail.com>
2021-06-02 15:22:57 +0300

    Add registering new tag feature

--- pm_proj.ino
+++ pm_proj.ino
@@ -47,7 +47,11 @@ MFRC522 rfid(SS_PIN, RST_PIN);
 byte state;
 const byte NO_MAX_TAGS = 5;
 byte NO_TAGS = 1;
-String tag_uids[NO_MAX_TAGS] = {"9A A5 D4 BF" /*, "75 14 69 21"*/};
+String tag_uids[NO_MAX_TAGS] = {"9A A5 D4 BF" /* , "75 14 69 21"*/};
+// Used for reading current tag UID
+String tag;
+// Register new tag
+boolean reg_new_tag = false;
 
 char correct_pass[4] = {'2', '7', '4', '0'};
 char password[4];
@@ -85,10 +89,13 @@ void setup() {
 
 }
 
-byte searchTagUID(String tag) {
+byte searchTagUID() {
+  Serial.print("search:");
   for (uint8_t j = 0; j < NO_TAGS; j++)
   {
-    if (tag.substring(1) == tag_uids[j])
+    Serial.print(tag_uids[j]);
+    Serial.print("\n");
+    if (tag == tag_uids[j])
       return 1;
   }
   return 0;
@@ -126,7 +133,10 @@ void denyTag() {
   digitalWrite(RED_LED, LOW);
   noTone(BUZZ_PIN);
   lcd.clear();
-  lcd.print("Scan Tag");
+  lcd.print("Register new tag?");
+  lcd.setCursor(0, 1);
+  lcd.print("1-yes,other-no");
+  state = 3;
 }
 
 void permitAccess() {
@@ -153,6 +163,66 @@ void denyAccess() {
   lcd.clear();
 }
 
+void readPassword() {
+  // Get key value if pressed
+  char key = kp.getKey();
+  
+  if (key)
+  {
+    if (key == '*' && count > 0)
+    {
+      deleteLastKey();
+    }
+    else if (key == '#' && count == 4)
+    {
+      pass_done = 1;
+    }
+    else if (count < 4)
+    {
+      Serial.print(key);
+      // Print character
+      lcd.print(key);
+      password[count] = key;
+      count++;
+    }
+  }
+}
+
+void registerNewTag() {
+  lcd.clear();
+  lcd.print("Correct password");
+  lcd.setCursor(0, 1);
+  lcd.print("Tag registered");
+  // save new tag
+  tag_uids[NO_TAGS] = tag;
+  NO_TAGS++;
+  // Allowed tags now:
+  Serial.print("Allowed tags now:");
+  for (uint8_t j = 0; j < NO_TAGS; j++)
+  {
+    Serial.print(tag_uids[j]);
+  }
+  Serial.print("\n");
+  
+  digitalWrite(GREEN_LED, HIGH);
+  delay(3000);
+  digitalWrite(GREEN_LED, LOW);
+  lcd.clear();
+}
+
+void denyRegisteringNewTag() {
+  lcd.clear();
+  lcd.print("Wrong password");
+  lcd.setCursor(0, 1);
+  lcd.print("Tag not registered");
+  digitalWrite(RED_LED, HIGH);
+  tone(BUZZ_PIN, 1000);
+  delay(3000);
+  digitalWrite(RED_LED, LOW);
+  noTone(BUZZ_PIN);
+  lcd.clear();
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
 
@@ -169,15 +239,16 @@ void loop() {
         return;
       }
       //Reading from the card
-      String tag = "";
+      tag = "";
       for (byte j = 0; j < rfid.uid.size; j++)
       {
         tag.concat(String(rfid.uid.uidByte[j] < 0x10 ? " 0" : " "));
         tag.concat(String(rfid.uid.uidByte[j], HEX));
       }
       tag.toUpperCase();
+      tag = tag.substring(1);
       Serial.print(tag);
-      if (!searchTagUID(tag))
+      if (!searchTagUID())
       {
         // Do not allow access
         denyTag();
@@ -188,32 +259,64 @@ void loop() {
       }
     }
 
-    
-  if (state == 2)
+  // verify password for registering new card
+  if (state == 3)
   {
-    // Get key value if pressed
-    char key = kp.getKey();
-
-  
-    if (key)
+    // option not chosen yet
+    if (!reg_new_tag)
     {
-      if (key == '*' && count > 0)
+      char key = kp.getKey();
+  
+      if (key)
       {
-        deleteLastKey();
-      }
-      else if (key == '#' && count == 4)
-      {
-        pass_done = 1;
-      }
-      else if (count < 4)
-      {
-        Serial.print(key);
-        // Print character
-        lcd.print(key);
-        password[count] = key;
-        count++;
+        // 1 - register new tag
+        if (key == '1')
+        {
+          reg_new_tag = true;
+          lcd.clear();
+          lcd.print("Enter Password");
+          lcd.setCursor(0, 1);
+        }
+        // other key - don't register
+        else
+        {
+          state = 1;
+          lcd.clear();
+          lcd.print("Scan Tag");
+        }
       }
     }
+    // option chosen - register the new tag
+    else
+    {
+      readPassword();
+      if (count == 4 && pass_done)
+      {
+        // reset counter
+        count = 0;
+        // reset flag
+        pass_done = 0;
+        delay(1000);
+        // If password is matched
+        if (!(strncmp(password, correct_pass, 4))) 
+        {
+          registerNewTag();
+        }
+        else
+        {
+          denyRegisteringNewTag();
+        }
+        state = 1;
+        lcd.clear();
+        lcd.print("Scan Tag");
+      }
+    }
+  }
+
+  // verify password
+  if (state == 2)
+  {
+    readPassword();
 
     // action after password is entered
     if (count == 4 && pass_done)
